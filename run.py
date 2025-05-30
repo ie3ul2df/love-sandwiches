180caab611f2e9c84de3a15b470659bbf564047f
arash12javadi <ie3ul2df@students.codeinstitute.net>
2025-05-30 20:07:05 +0100

    access granted to google sheet from python

---
+++ run.py
@@ -0,0 +1,18 @@
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+CREDS_SCOPED = CREDS.with_scopes(SCOPE)
+GSPRED_CLIENT = gspread.authorize(CREDS_SCOPED)
+SHEET = GSPRED_CLIENT.open("love_sandwiches")
+
+sales = SHEET.worksheet("sales")
+data = sales.get_all_values()
+
+print(data)
