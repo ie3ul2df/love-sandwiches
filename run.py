b344b162e7c0525d810e0e51aca208ba7d34a346
arash12javadi <ie3ul2df@students.codeinstitute.net>
2025-05-30 22:05:44 +0100

    created the basic function to get data from user

--- run.py
+++ run.py
@@ -12,7 +12,22 @@ CREDS_SCOPED = CREDS.with_scopes(SCOPE)
 GSPRED_CLIENT = gspread.authorize(CREDS_SCOPED)
 SHEET = GSPRED_CLIENT.open("love_sandwiches")
 
-sales = SHEET.worksheet("sales")
-data = sales.get_all_values()
+# sales = SHEET.worksheet("sales")
+# data = sales.get_all_values()
 
-print(data)
+# print(data)
+
+def get_sales_data():
+    """
+    Get sales data from the sales worksheet.
+    Returns a list of lists containing the sales data.
+    """
+    print("\n")
+    print("Please enter sales data from the last market.")
+    print("Data should be six numbers, separated by commas.")
+    print("Example: 10,20,30,40,50,60\n")
+
+    data_str = input("Enter your data here: ")
+    print(f"You entered: {data_str}\n")
+
+get_sales_data()
